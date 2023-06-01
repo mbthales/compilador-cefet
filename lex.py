8d805998c2e6af7c716167b4838d2fe18ad2a49a
Thales M <mbthales10@gmail.com>
2023-06-01 19:50:30 -0300

    Handle error cases and update logic

--- lex.py
+++ lex.py
@@ -6,12 +6,10 @@ tokens = (
     'ID',
     'VARIABLE_NAME',
     'SYMBOL',
-    'NUMBER',
-    'TEXT',
+    'TERM',
 )
 
-t_NUMBER = r'\d+'
-t_TEXT = r'\".*\"'
+t_TERM = r'"[a-zA-Z_][a-zA-Z_0-9]*"|\d+'
 t_ignore = ' \t'
 
 def t_SYMBOL(t):
@@ -27,10 +25,12 @@ def t_VARIABLE_NAME(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+    
+class LexicalError(Exception):
+    pass
 
 def t_error(t):
-    print(f"Illegal character '{t.value[0]}'")
-    t.lexer.skip(1)
+    raise LexicalError(f"Lexical error: Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
 
 lexer = lex.lex()
 
--- yacc.py
+++ yacc.py
@@ -5,18 +5,22 @@ from lex import tokens
 variables = {}
 
 def p_expression_create_variable(p):
-    '''expression : ID VARIABLE_NAME SYMBOL term
+    '''expression : ID VARIABLE_NAME SYMBOL TERM
     | ID VARIABLE_NAME SYMBOL expression'''
-    variables[p[2]] = p[4]
+    if p[3] != "=" or p[1] != "var":
+        p_error(p)
+    else:
+        variables[p[2]] = p[4]
 
 def p_expression_function_call(p):
-    '''expression : ID SYMBOL term SYMBOL
+    '''expression : ID SYMBOL TERM SYMBOL
+    | ID SYMBOL VARIABLE_NAME SYMBOL
     | ID SYMBOL expression SYMBOL'''
     if p[1] == 'mostrar':
         if p[3] in variables:
-            print(variables[p[3]])
+            print(variables[p[3]].strip('"'))
         else:
-            print(p[3])
+            print(p[3].strip('"'))
     elif p[1] == 'maiusculo':
         if p[3] in variables:
             p[0] = variables[p[3]].upper()
@@ -28,18 +32,9 @@ def p_expressions(p):
     | expressions expression'''
     pass
 
-def p_symbol(p):
-    'symbol : SYMBOL'
-    p[0] = p[1]
-
-def p_term(p):
-    '''term : NUMBER 
-    | TEXT
-    | VARIABLE_NAME'''
-    p[0] = p[1]
-
 def p_error(p):
     print("Syntax error in input!")
+    raise SyntaxError()
 
 parser = yacc.yacc(start='expressions')
 data = open('teste.txt', 'r')
